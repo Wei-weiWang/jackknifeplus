f4ea875581a478170dca59d86d67676a489f804e
Weiwei Wang <weiweiwangnk@gmail.com>
2021-11-23 15:08:11 -0600

    Change code style

--- R/Functions.R
+++ R/Functions.R
@@ -19,20 +19,20 @@
 #   return(Xtest%*%lm(Ytrain~Xtrain)$coefficients)
 # }
 
-# Calculate celing(1-a)*(n+1) quantlile of a n vector  
+# Calculate celing(1-a)*(n+1) quantlile of a n vector
 # quantile_n_a = function(A, a){
 #   n = length(A)
 #   return(sort(A)[ceiling((1-a)*(n+1))])##this is null because of ceiling
 # }
-#(-q_n_a(-q1),q_n_a(q2))
+# (-q_n_a(-q1),q_n_a(q2))
 
-jackknifeplus <- function(Xtrain, Ytrain, Xtest, Ytest, predfunction, a){
-  n = nrow(Xtrain)
+jackknifeplus <- function(Xtrain, Ytrain, Xtest, Ytest, predfunction, a) {
+  n <- nrow(Xtrain)
   for (i in 1:n) {
-    predY = predfunction(Xtrain[-i,],Ytrain[-i],Xtrain[i,])#trainfunction and output leave one out residual
-    Results_RLOO[i] = abs(Ytrain[i] - predY)
-    q1[i] = predfunction(Xtrain[-i,],Ytrain[-i],Xtest) - Results_RLOO[i]# jackknife+ vector
-    q2[i] = predfunction(Xtrain[-i,],Ytrain[-i],Xtest) + Results_RLOO[i]
+    predY <- predfunction(Xtrain[-i, ], Ytrain[-i], Xtrain[i, ]) # trainfunction and output leave one out residual
+    Results_RLOO[i] <- abs(Ytrain[i] - predY)
+    q1[i] <- predfunction(Xtrain[-i, ], Ytrain[-i], Xtest) - Results_RLOO[i] # jackknife+ vector
+    q2[i] <- predfunction(Xtrain[-i, ], Ytrain[-i], Xtest) + Results_RLOO[i]
     return(q1)
   }
 }

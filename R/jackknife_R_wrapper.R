240ccc64ec7bb71bc294be0a85fd3865afa7c5b3
Weiwei Wang <weiweiwangnk@gmail.com>
2021-12-13 16:20:05 -0600

    Update parameter description

--- R/jackknife_R_wrapper.R
+++ R/jackknife_R_wrapper.R
@@ -1,11 +1,11 @@
 #'jackknife_c_wrapper
 #'
-#' @param Xtrain #' n training samples, whose format must be consistent with input of predfunction user provides 
-#' @param Ytrain #' a response vector of size n
-#' @param Xtest #' a test sample, whose format must be consistent with input of predfunction user provides
-#' @param a #' probability coverage is 1-2a 
+#' @param Xtrain #' n training samples, whose format must be matrix n*p 
+#' @param Ytrain #' a response matrix of dimension n * 1
+#' @param Xtest #' a test sample as matrix, whose format must be consistent with Xtrain 1*p
+#' @param a #' probability coverage is 1-2a 0<a<1 
 #'
-#' @return #' conformal confidence interval of jackknife+
+#' @return #' conformal confidence interval of jackknife
 #' @export
 #'
 #' @examples 
--- R/jackknifeplus_R_wrapper.R
+++ R/jackknifeplus_R_wrapper.R
@@ -1,14 +1,18 @@
 #'jackknifeplus_c_wrapper
 #'
-#' @param Xtrain #' n training samples, whose format must be consistent with input of predfunction user provides 
-#' @param Ytrain #' a response vector of size n
-#' @param Xtest #' a test sample, whose format must be consistent with input of predfunction user provides
-#' @param a #' probability coverage is 1-2a 
+#' @param Xtrain #' n training samples, whose format must be matrix n*p 
+#' @param Ytrain #' a response matrix of dimension n * 1
+#' @param Xtest #' a test sample as matrix, whose format must be consistent with Xtrain 1*p
+#' @param a #' probability coverage is 1-2a 0<a<1 
 #'
 #' @return #' conformal confidence interval of jackknife+
 #' @export
 #'
-#' @examples #' jackknifeplus_c_wrapper(Xtrain, Ytrain, Xtest, 0.05)
+#' @examples 
+#' Xtrain=matrix(rnorm(200),40,5)
+#' Ytrain=matrix(rnorm(40),40,1)
+#' Xtest=matrix(rnorm(5),1,5)
+#' result = jackknifeplus_c_wrapper(Xtrain, Ytrain, Xtest, 0.05)
 #' 
  
 
